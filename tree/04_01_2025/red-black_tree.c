7b15bf439975be9e2ea94c9422d54ad883df6b28
Arunachalam-286 <aruntdy286@gmail.com>
2025-01-04 14:22:21 +0530

    changes

--- tree/04_01_2025/red-black_tree.c
+++ tree/04_01_2025/red-black_tree.c
@@ -9,8 +9,9 @@ struct Node {
     struct Node *right;
     struct Node *parent;
     enum Color color;
-} *root = NULL, *TNULL;
+}*root = NULL, *TNULL;
 
+// Initialize the TNULL sentinel node
 void initializeTNULL() {
     TNULL = (struct Node*)malloc(sizeof(struct Node));
     TNULL->color = BLACK;
@@ -19,6 +20,7 @@ void initializeTNULL() {
     TNULL->parent = NULL;
 }
 
+// Left Rotate: Used to maintain Red-Black Tree properties
 void leftRotate(struct Node *x) {
     struct Node *y = x->right;
     x->right = y->left;
@@ -37,6 +39,7 @@ void leftRotate(struct Node *x) {
     x->parent = y;
 }
 
+// Right Rotate: Used to maintain Red-Black Tree properties
 void rightRotate(struct Node *x) {
     struct Node *y = x->left;
     x->left = y->right;
@@ -55,6 +58,7 @@ void rightRotate(struct Node *x) {
     x->parent = y;
 }
 
+// Fix Red-Black Tree violations after insertion
 void fixInsert(struct Node *k) {
     struct Node *u;
     while (k->parent->color == RED) {
@@ -98,6 +102,7 @@ void fixInsert(struct Node *k) {
     root->color = BLACK;
 }
 
+// Insertion: Insert a node into the Red-Black Tree
 void insert(int key) {
     struct Node *node = (struct Node*)malloc(sizeof(struct Node));
     node->parent = NULL;
@@ -139,6 +144,7 @@ void insert(int key) {
     fixInsert(node);
 }
 
+// Inorder Traversal
 void inorder(struct Node *node) {
     if (node != TNULL) {
         inorder(node->left);
@@ -147,17 +153,133 @@ void inorder(struct Node *node) {
     }
 }
 
+// Fix the tree after deleting a node
+void fixDelete(struct Node *x) {
+    struct Node *s;
+    while (x != root && x->color == BLACK) {
+        if (x == x->parent->left) {
+            s = x->parent->right;
+            if (s->color == RED) {
+                s->color = BLACK;
+                x->parent->color = RED;
+                leftRotate(x->parent);
+                s = x->parent->right;
+            }
+            if (s->left->color == BLACK && s->right->color == BLACK) {
+                s->color = RED;
+                x = x->parent;
+            } else {
+                if (s->right->color == BLACK) {
+                    s->left->color = BLACK;
+                    s->color = RED;
+                    rightRotate(s);
+                    s = x->parent->right;
+                }
+                s->color = x->parent->color;
+                x->parent->color = BLACK;
+                s->right->color = BLACK;
+                leftRotate(x->parent);
+                x = root;
+            }
+        } else {
+            s = x->parent->left;
+            if (s->color == RED) {
+                s->color = BLACK;
+                x->parent->color = RED;
+                rightRotate(x->parent);
+                s = x->parent->left;
+            }
+            if (s->right->color == BLACK && s->left->color == BLACK) {
+                s->color = RED;
+                x = x->parent;
+            } else {
+                if (s->left->color == BLACK) {
+                    s->right->color = BLACK;
+                    s->color = RED;
+                    leftRotate(s);
+                    s = x->parent->left;
+                }
+                s->color = x->parent->color;
+                x->parent->color = BLACK;
+                s->left->color = BLACK;
+                rightRotate(x->parent);
+                x = root;
+            }
+        }
+    }
+    x->color = BLACK;
+}
+
+// Delete a node from the Red-Black Tree
+void deleteNode(int key) {
+    struct Node *z = root;
+    struct Node *x, *y;
+    while (z != TNULL) {
+        if (z->data == key) {
+            break;
+        }
+        if (key < z->data) {
+            z = z->left;
+        } else {
+            z = z->right;
+        }
+    }
+
+    if (z == TNULL) {
+        printf("Node with value %d not found.\n", key);
+        return;
+    }
+
+    y = z;
+    int yOriginalColor = y->color;
+    if (z->left == TNULL) {
+        x = z->right;
+        transplant(z, z->right);
+    } else if (z->right == TNULL) {
+        x = z->left;
+        transplant(z, z->left);
+    } else {
+        y = minimum(z->right);
+        yOriginalColor = y->color;
+        x = y->right;
+        if (y->parent == z) {
+            x->parent = y;
+        } else {
+            transplant(y, y->right);
+            y->right = z->right;
+            y->right->parent = y;
+        }
+        transplant(z, y);
+        y->left = z->left;
+        y->left->parent = y;
+        y->color = z->color;
+    }
+    if (yOriginalColor == BLACK) {
+        fixDelete(x);
+    }
+}
+
 int main() {
     initializeTNULL();
 
     int val;
+    printf("Enter values to insert into the Red-Black Tree (-1 to stop):\n");
     do {
         scanf("%d", &val);
         if (val != -1)
             insert(val);
     } while (val != -1);
 
-    printf("Inorder Traversal: ");
+    printf("Inorder Traversal after insertions: ");
+    inorder(root);
+    printf("\n");
+
+    int delVal;
+    printf("Enter value to delete from the tree: ");
+    scanf("%d", &delVal);
+    deleteNode(delVal);
+
+    printf("Inorder Traversal after deletion: ");
     inorder(root);
     printf("\n");
 
