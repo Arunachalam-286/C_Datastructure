88b452bc9bba30abd3a575208b756707bba390a3
Arunachalam-286 <aruntdy286@gmail.com>
2025-01-04 11:47:45 +0530

    changes

---
+++ tree/04_01_2025/Count_the_number_of_words_starting_with_a_given_prefix_in_a_trie.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+}*root = NULL;
+
+void insert(struct Node **node, int val) {
+    //1. If the current node is NULL, create a new node
+    if (*node == NULL) {
+        struct Node *newnode = (struct Node*)malloc(sizeof(struct Node));
+        newnode->data = val;
+        newnode->left = NULL;
+        newnode->right = NULL;
+        *node = newnode;
+        return;
+    }
+    //2. Insert into the left or right subtree based on value
+    if (val < (*node)->data) {
+        insert(&((*node)->left), val);
+    } else {
+        insert(&((*node)->right), val);
+    }
+}
+
+void inorder(struct Node *node) {
+    if (node == NULL) return;
+    inorder(node->left);
+    printf("%d ", node->data);
+    inorder(node->right);
+}
+
+int main() {
+    int val;
+    do {
+        scanf("%d", &val);
+        if (val != -1)
+            insert(&root, val);
+    } while (val != -1);
+
+    printf("Inorder Traversal: ");
+    inorder(root);
+    printf("\n");
+    return 0;
+}
